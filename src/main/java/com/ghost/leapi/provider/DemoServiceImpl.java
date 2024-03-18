e27bb14ee1ba7bcb8beb59420b2317283e646e37
乐小鑫 <115512481+lvxinxinL@users.noreply.github.com>
2024-03-18 20:42:20 +0800

    Update DemoServiceImpl.java

--- src/main/java/com/ghost/leapi/provider/DemoServiceImpl.java
+++ src/main/java/com/ghost/leapi/provider/DemoServiceImpl.java
@@ -12,12 +12,9 @@ public class DemoServiceImpl implements DemoService {
         return "Hello " + name;
     }
 
-    // [编程学习交流圈](https://www.code-nav.cn/) 连接万名编程爱好者，一起优秀！20000+ 小伙伴交流分享、40+ 大厂嘉宾一对一答疑、100+ 各方向编程交流群、4000+ 编程问答参考
-
     @Override
     public String sayHello2(String name) {
         return "ghost";
     }
-
 
 }
